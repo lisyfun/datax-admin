ca705332e1507d07039db18c60009482bbe0f812
lisongyu <li.songyu@qq.com>
2025-01-17 14:21:39 +0800

    v0.1.41 add build images shell

---
+++ build_amd64.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 设置错误时退出
+set -e
+
+echo "=== 开始构建 DataX-Admin ==="
+
+# 构建前端
+echo "=== 开始构建前端 ==="
+cd frontend
+
+echo "构建前端项目..."
+pnpm build
+
+cd ..
+
+# 构建后端
+echo "=== 开始构建镜像 ==="
+docker build --platform=linux/amd64 -f Dockerfile -t datax-admin .
+echo "=== 构建完成 ==="
